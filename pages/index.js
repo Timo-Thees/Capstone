7df534cbe7c74c532ce85a0df2df4f09cb40ed13
Timo Thees <timo.thees@gmx.net>
2022-11-30 16:20:46 +0100

    bug hunt

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,6 @@ import Editor from "../components/Editor";
 function saveText(title = "Unnamed Document", text) {
   localStorage.setItem(`${title}`, `${text}`);
 }
-
 export default function Home() {
   return (
     <div>
