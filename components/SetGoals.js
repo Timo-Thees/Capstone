31488f8b1770a55dcb27b7d572dbf3ce00a84c01
Timo Thees <timo.thees@gmx.net>
2022-12-02 16:42:25 +0100

    first test goal page

--- components/SetGoals.js
+++ components/SetGoals.js
@@ -1,9 +1,36 @@
+import {useState} from "react";
+
 export default function SetGoals({page}) {
+  const [writingTime, setWritingTime] = useState({
+    Mon: 0,
+    Tue: 0,
+    Wen: 0,
+    Thu: 0,
+    Fri: 0,
+    Sat: 0,
+    Sun: 0,
+  });
+
   if (page === "goals") {
     return (
       <>
-        <h1>Write now!</h1>
-        <p>The app to help you develop a writing habit</p>
+        <label>How many words do you want to write each day?</label>
+        <input type="text"></input>
+        <ul>
+          <li>
+            {" "}
+            Monday <input type="radio"></input>
+            <p>{writingTime.Mon}</p>
+            <button
+              onClick={() =>
+                setWritingTime({...writingTime, Mon: writingTime.Mon + 1})
+              }
+            >
+              +
+            </button>
+            <button onClick={() => console.log(writingTime.Mon)}>show</button>
+          </li>
+        </ul>
       </>
     );
   } else {
