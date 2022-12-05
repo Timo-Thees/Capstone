7084457d08a6ee73aff9863245007ef8188e02e6
Timo Thees <timo.thees@gmx.net>
2022-12-05 12:59:18 +0100

    add method to create more timeslots

--- components/SetGoals.js
+++ components/SetGoals.js
@@ -1,8 +1,17 @@
 import {useState} from "react";
+import Timeslot from "./Timeslot";
 
 export default function SetGoals({page}) {
-  const [writingTime, setWritingTime] = useState([]);
-
+  const [writingTime, setWritingTime] = useState([{weekday: "never", key: 0}]);
+  function handleNewTimeslot(Day) {
+    const lastEntry = writingTime[writingTime.length - 1];
+    const newKey = lastEntry.key + 1;
+    setWritingTime([...writingTime, {weekday: Day, key: newKey}]);
+  }
+  function handleDeleteTimeslot(keyForDeletion) {
+    event.preventDefault();
+    console.log(writingTime.find(keyForDeletion));
+  }
   if (page === "goals") {
     return (
       <>
@@ -12,14 +21,18 @@ export default function SetGoals({page}) {
           <li>
             {" "}
             Monday <input type="radio"></input>
-            {writingTime.map(day => {
-              if (day === "Mon") {
-                return <button key={day.index}>that was easy!</button>;
+            {writingTime.map(timeslot => {
+              if (timeslot.weekday === "Mon") {
+                return (
+                  <Timeslot
+                    key={timeslot.key}
+                    handleDeleteTimeslot={handleDeleteTimeslot}
+                    keyForDeletion={timeslot.key}
+                  />
+                );
               }
             })}
-            <button onClick={() => setWritingTime([...writingTime, "Mon"])}>
-              +
-            </button>
+            <button onClick={() => handleNewTimeslot("Mon")}>+</button>
             <button onClick={() => console.log(writingTime)}>show</button>
           </li>
         </ul>
